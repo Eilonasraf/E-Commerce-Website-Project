08efc4f49073f2611ec56051c40ebe195de45510
Eilonasraf <103586426+Eilonasraf@users.noreply.github.com>
2024-05-27 22:56:42 +0300

    Delete routes directory

--- routes/adminRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const adminController = require('../controller/adminController');
-const router = express.Router();
-
-router.get('/graphs', adminController.renderGraphsPage);
-
-module.exports = router;
