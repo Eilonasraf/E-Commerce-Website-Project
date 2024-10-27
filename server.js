370ae642744a43635308d29abf92305a4450375d
Eilonasraf <eilonasraf@gmail.com>
2024-10-27 12:56:39 +0200

    Azure changes

--- server.js
+++ server.js
@@ -44,10 +44,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // MongoDB Connection
-console.log(process.env.MONGODB_URI);
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('Connected to MongoDB'))
-    .catch((error) => console.error('Failed to connect to MongoDB:', error));
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  ssl: true
+})
+.then(() => console.log('Connected to Cosmos DB'))
+.catch((error) => console.error('Failed to connect to Cosmos DB:', error));
+
 
 // Set up EJS view engine and views directory
 app.set('view engine', 'ejs');
